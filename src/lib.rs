a1140792cb5719958d0ac7565fcf9ea6a50b59d9
linux_china <libing.chen@gmail.com>
2021-10-13 12:44:26 -0700

    Add deno_bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use deno_bindgen::deno_bindgen;
+
 #[no_mangle]
 pub extern "C" fn add(a: isize, b: isize) -> isize {
     a + b
@@ -7,6 +9,19 @@ pub extern "C" fn add(a: isize, b: isize) -> isize {
 pub extern "C" fn print_buffer(ptr: *const u8, len: usize) {
     let buf = unsafe { std::slice::from_raw_parts(ptr, len) };
     println!("{:?}", buf);
+}
+
+#[deno_bindgen]
+pub struct Input {
+    a: i32,
+    b: i32,
+    c: String,
+}
+
+#[deno_bindgen]
+fn sum(input: Input) -> i32 {
+    println!("{}", input.c);
+    input.a + input.b
 }
 
 #[cfg(test)]
