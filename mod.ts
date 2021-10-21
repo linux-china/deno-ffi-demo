9fff1ab7601c3593a72779c1006d8af2cfc01784
linux_china <libing.chen@gmail.com>
2021-10-21 12:06:21 -0700

    Adjust code

--- mod.ts
+++ mod.ts
@@ -1,46 +1,14 @@
-// Determine library extension based on your OS.
-let ext = "";
-switch (Deno.build.os) {
-    case "windows":
-        ext = "dll";
-        break;
-    case "darwin":
-        ext = "dylib";
-        break;
-    case "linux":
-        ext = "so";
-        break;
-}
-
-const libName = `target/debug/libdeno_ffi.${ext}`;
-// Open library and define exported symbols
-const dylibRs = Deno.dlopen(libName, {
-    add: {
-        parameters: ["isize", "isize"],
-        result: "isize"
-    },
-    print_buffer: {
-        parameters: ["buffer", "usize"],
-        result: "void",
-    }
-});
-
-// Open library and define exported symbols
-const dylibC = Deno.dlopen(`target/lib_add.${ext}`, {
-    add: {
-        parameters: ["isize", "isize"],
-        result: "isize"
-    }
-});
+// add.ts
+import * as bindings from "./bindings/bindings.ts";
 
 export function add(a: number, b: number): number {
-    return dylibRs.symbols.add(a, b) as number;
+    return bindings.sum({
+        a,
+        b,
+        c: "sum"
+    }) as number;
 }
 
-export function printBuffer(buffer: Uint8Array) {
-    dylibRs.symbols.print_buffer(buffer, buffer.length);
-}
-
-export function add2(a: number, b: number): number {
-    return dylibC.symbols.add(a, b) as number;
+export function sayHello(name: string) {
+    return bindings.say_hello(name);
 }
