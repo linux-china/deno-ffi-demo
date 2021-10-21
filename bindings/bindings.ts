3f51626fbc68381245719e2521cc8d0010f1d6d4
linux_china <libing.chen@gmail.com>
2021-10-21 12:05:46 -0700

    regenerated

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,10 +1,35 @@
 // Auto-generated with deno_bindgen
+import {Plug} from "https://deno.land/x/plug@0.4.0/mod.ts"
 
-const encode = (s: string) => new TextEncoder().encode(s);
-const _lib = Deno.dlopen("target/debug/libdeno_ffi.dylib", { sum: { parameters: [ "buffer", "usize" ], result: "i32" } });
-type Input = { a: number; b: number; c: any };
-export function sum(a0: Input) {
-  const a0_buf = encode(JSON.stringify(a0));
-  return _lib.symbols.sum(a0_buf, a0_buf.byteLength) as number
+function encode(v: string | Uint8Array): Uint8Array {
+    if (typeof v !== "string") return v
+    return new TextEncoder().encode(v)
 }
- 
+
+const opts = {
+    name: "deno_ffi",
+    url: "target/debug",
+}
+const _lib = await Plug.prepare(opts, {
+    sum: {parameters: ["buffer", "usize"], result: "i32", nonblocking: false},
+    say_hello: {
+        parameters: ["buffer", "usize"],
+        result: "void",
+        nonblocking: false,
+    },
+})
+export type Input = {
+    a: number
+    b: number
+    c: string
+}
+
+export function sum(a0: Input) {
+    const a0_buf = encode(JSON.stringify(a0))
+    return _lib.symbols.sum(a0_buf, a0_buf.byteLength) as number
+}
+
+export function say_hello(a0: string) {
+    const a0_buf = encode(a0)
+    return _lib.symbols.say_hello(a0_buf, a0_buf.byteLength) as null
+}
