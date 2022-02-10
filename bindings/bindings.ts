ec1a65c6342ca33609da4030fa21bc5703bc03c5
linux_china <libing.chen@gmail.com>
2022-02-10 15:56:22 -0800

    Update to deno_bindgen 0.5.0

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,21 +1,33 @@
 // Auto-generated with deno_bindgen
-import { CachePolicy, prepare } from "https://deno.land/x/plug@0.4.1/plug.ts"
+import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.1/plug.ts"
 function encode(v: string | Uint8Array): Uint8Array {
   if (typeof v !== "string") return v
   return new TextEncoder().encode(v)
 }
+function decode(v: Uint8Array): string {
+  return new TextDecoder().decode(v)
+}
+function readPointer(v: any): Uint8Array {
+  const ptr = new Deno.UnsafePointerView(v as Deno.UnsafePointer)
+  const lengthBe = new Uint8Array(4)
+  const view = new DataView(lengthBe.buffer)
+  ptr.copyInto(lengthBe, 0)
+  const buf = new Uint8Array(view.getUint32(0))
+  ptr.copyInto(buf, 4)
+  return buf
+}
 const opts = {
   name: "deno_ffi",
-  url: (new URL("../target/release", import.meta.url)).toString(),
-  policy: undefined,
+  url: (new URL("../target/debug", import.meta.url)).toString(),
+  policy: CachePolicy.NONE,
 }
 const _lib = await prepare(opts, {
   say_hello: {
-    parameters: ["buffer", "usize"],
+    parameters: ["pointer", "usize"],
     result: "void",
     nonblocking: false,
   },
-  sum: { parameters: ["buffer", "usize"], result: "i32", nonblocking: false },
+  sum: { parameters: ["pointer", "usize"], result: "i32", nonblocking: false },
 })
 export type Input = {
   a: number
@@ -24,9 +36,13 @@ export type Input = {
 }
 export function say_hello(a0: string) {
   const a0_buf = encode(a0)
-  return _lib.symbols.say_hello(a0_buf, a0_buf.byteLength) as null
+  let rawResult = _lib.symbols.say_hello(a0_buf, a0_buf.byteLength)
+  const result = rawResult
+  return result
 }
 export function sum(a0: Input) {
   const a0_buf = encode(JSON.stringify(a0))
-  return _lib.symbols.sum(a0_buf, a0_buf.byteLength) as number
+  let rawResult = _lib.symbols.sum(a0_buf, a0_buf.byteLength)
+  const result = rawResult
+  return result
 }
