291647e9aa30b66f626f34e359f87618f2acf7d6
linux_china <libing.chen@gmail.com>
2022-09-29 13:03:45 +0800

    chore: updated

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,14 +1,17 @@
 // Auto-generated with deno_bindgen
-import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.1/plug.ts"
+import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.2/plug.ts"
+
 function encode(v: string | Uint8Array): Uint8Array {
   if (typeof v !== "string") return v
   return new TextEncoder().encode(v)
 }
+
 function decode(v: Uint8Array): string {
   return new TextDecoder().decode(v)
 }
+
 function readPointer(v: any): Uint8Array {
-  const ptr = new Deno.UnsafePointerView(v as Deno.UnsafePointer)
+  const ptr = new Deno.UnsafePointerView(v as bigint)
   const lengthBe = new Uint8Array(4)
   const view = new DataView(lengthBe.buffer)
   ptr.copyInto(lengthBe, 0)
@@ -16,10 +19,31 @@ function readPointer(v: any): Uint8Array {
   ptr.copyInto(buf, 4)
   return buf
 }
+
+const url = new URL("../target/release", import.meta.url)
+let uri = url.toString()
+if (!uri.endsWith("/")) uri += "/"
+
+let darwin: string | { aarch64: string; x86_64: string } = uri
+  + "libdeno_ffi.dylib"
+
+if (url.protocol !== "file:") {
+  // Assume that remote assets follow naming scheme
+  // for each macOS artifact.
+  darwin = {
+    aarch64: uri + "libdeno_ffi_arm64.dylib",
+    x86_64: uri + "libdeno_ffi.dylib",
+  }
+}
+
 const opts = {
   name: "deno_ffi",
-  url: (new URL("../target/debug", import.meta.url)).toString(),
-  policy: CachePolicy.NONE,
+  urls: {
+    darwin,
+    windows: uri + "deno_ffi.dll",
+    linux: uri + "libdeno_ffi.so",
+  },
+  policy: undefined,
 }
 const _lib = await prepare(opts, {
   say_hello: {
@@ -36,13 +60,15 @@ export type Input = {
 }
 export function say_hello(a0: string) {
   const a0_buf = encode(a0)
-  let rawResult = _lib.symbols.say_hello(a0_buf, a0_buf.byteLength)
+  const a0_ptr = Deno.UnsafePointer.of(a0_buf)
+  let rawResult = _lib.symbols.say_hello(a0_ptr, a0_buf.byteLength)
   const result = rawResult
   return result
 }
 export function sum(a0: Input) {
   const a0_buf = encode(JSON.stringify(a0))
-  let rawResult = _lib.symbols.sum(a0_buf, a0_buf.byteLength)
+  const a0_ptr = Deno.UnsafePointer.of(a0_buf)
+  let rawResult = _lib.symbols.sum(a0_ptr, a0_buf.byteLength)
   const result = rawResult
   return result
 }
